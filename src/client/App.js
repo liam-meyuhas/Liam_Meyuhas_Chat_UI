52205f7761c0555a80d2e0676ec827e587542de3
noam <noam.segev940@gmail.com>
2025-01-29 11:39:27 +0200

    add transition to sidebar

--- src/client/App.js
+++ src/client/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import ChatInput from './Components/ChatInput/ChatInput';
 import alpha from './images/alpha.png';
@@ -9,27 +9,28 @@ import UserIcon from './Components/UserIcon/UserIcon';
 import ImageResponse from './Components/ImageResponse/ImageResponse';
 import VideoResponse from './Components/VideoResponse/VideoResponse';
 import SplashScreen from './Components/SplashScreen/SplashScreen';
+import clsx from 'clsx';
 
 function App() {
   const [faq, setFaq] = useState({
-    "What time is it?": "The current time is...",
-    "מה מזג האוויר?": "מזג האוויר כרגע הוא...",
-    "What is your name?": "My name is Alpha",
-    "מהי בירת ישראל?": "בירת ישראל היא ירושלים",
-    "איך קוראים לך?": "השם שלי הוא אלפא",
-    "What is the capital of Israel?": "The capital of Israel is Jerusalem.",
-    "תן לי רעיון לאפליקציה":
-      "מה דעתך על אפליקציה לארגון מטלות יומית עם תזכורות חכמות?",
-    "היכרות עם כוכב נולד":
-      "כוכב נולד הוא העוזר האישי שלך שמיועד להעשיר את חוויית השימוש שלך ולספק מידע מגוון.",
-    "למה כדאי לי להשתמש בכוכב נולד":
-      "כוכב נולד הוא העוזר האישי המושלם, המספק מידע, עוזר במשימות יומיות, ומציע רעיונות והשראה בכל נושא.",
-    "רעיון לשאלה": "מה דעתך על השאלה: 'איך כדאי להתכונן לריאיון עבודה?'",
-    "תייצר לי תמונה של שועל מטייל ביער, צבעוני, זווית צילום רחבה": (
-      <ImageResponse />
+    'What time is it?': 'The current time is...',
+    'מה מזג האוויר?': 'מזג האוויר כרגע הוא...',
+    'What is your name?': 'My name is Alpha',
+    'מהי בירת ישראל?': 'בירת ישראל היא ירושלים',
+    'איך קוראים לך?': 'השם שלי הוא אלפא',
+    'What is the capital of Israel?': 'The capital of Israel is Jerusalem.',
+    'תן לי רעיון לאפליקציה':
+      'מה דעתך על אפליקציה לארגון מטלות יומית עם תזכורות חכמות?',
+    'היכרות עם כוכב נולד':
+      'כוכב נולד הוא העוזר האישי שלך שמיועד להעשיר את חוויית השימוש שלך ולספק מידע מגוון.',
+    'למה כדאי לי להשתמש בכוכב נולד':
+      'כוכב נולד הוא העוזר האישי המושלם, המספק מידע, עוזר במשימות יומיות, ומציע רעיונות והשראה בכל נושא.',
+    'רעיון לשאלה': 'מה דעתך על השאלה: \'איך כדאי להתכונן לריאיון עבודה?\'',
+    'תייצר לי תמונה של שועל מטייל ביער, צבעוני, זווית צילום רחבה': (
+      <ImageResponse/>
     ),
-    "כתוב לי קוד בפייתון": `('ברוכים הבאים לכוכב נולד⭐')print //הפקודה תדפיס ברוכים הבאים לכוכב נולד⭐`,
-    "תייצר לי סרטון קצר של דגל ישראל מתנופף ברוח": <VideoResponse />,
+    'כתוב לי קוד בפייתון': `('ברוכים הבאים לכוכב נולד⭐')print //הפקודה תדפיס ברוכים הבאים לכוכב נולד⭐`,
+    'תייצר לי סרטון קצר של דגל ישראל מתנופף ברוח': <VideoResponse/>
   });
 
   const [response, setResponse] = useState([]);
@@ -40,44 +41,44 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [id, setId] = useState(1);
   const [isLightMode, setIsLightMode] = useState();
-  const [botName, setBotName] = useState("");
-  const [fname, setFname] = useState("");
+  const [botName, setBotName] = useState('');
+  const [fname, setFname] = useState('');
 
-  const initalUserPath = "ליאם מיוחס / יחידת אוצר / צוות מטמון";
+  const initalUserPath = 'ליאם מיוחס / יחידת אוצר / צוות מטמון';
 
   const initalBotName = (initalUserPath) => {
-    const userPath = initalUserPath.split("/");
+    const userPath = initalUserPath.split('/');
 
     return userPath[0].trim();
   };
 
   const initialName = initalBotName(initalUserPath);
 
-  const [timeOfDay, setTimeOfDay] = useState("");
+  const [timeOfDay, setTimeOfDay] = useState('');
 
   const getTimeOfDay = () => {
     const hours = new Date().getHours();
 
     if (hours >= 0 && hours < 12) {
-      setTimeOfDay(" בוקר טוב ");
+      setTimeOfDay(' בוקר טוב ');
     } else if (hours >= 12 && hours < 18) {
-      setTimeOfDay(" צהריים טובים ");
+      setTimeOfDay(' צהריים טובים ');
     } else {
-      setTimeOfDay(" ערב טוב ");
+      setTimeOfDay(' ערב טוב ');
     }
   };
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/botname", {
-      method: "PUT",
+    fetch('http://localhost:5000/api/botname', {
+      method: 'PUT',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ fname: initialName }),
+      body: JSON.stringify({fname: initialName})
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Error updating full name");
+          throw new Error('Error updating full name');
         }
         return response.json();
       })
@@ -87,7 +88,7 @@ function App() {
         setIsLightMode(data.bot.isLightMode);
       })
       .catch((error) => {
-        console.error("Error updating full name:", error);
+        console.error('Error updating full name:', error);
       });
 
     getTimeOfDay();
@@ -104,7 +105,7 @@ function App() {
       const chat = {
         id: id,
         responses: response,
-        timeStamp: new Date(),
+        timeStamp: new Date()
       };
       setId(id + 1);
       setAllChats([chat, ...allChats]);
@@ -117,11 +118,11 @@ function App() {
   };
 
   return (
-    <div className={`App ${isLightMode ? "light-mode" : ""}`} data-testid="app">
-      <SplashScreen isLoading={isLoading} setIsLoading={setIsLoading} />
+    <div className={`App ${isLightMode ? 'light-mode' : ''}`} data-testid="app">
+      <SplashScreen isLoading={isLoading} setIsLoading={setIsLoading}/>
       <div className="header-logo">
         <header className="logo">
-          <img src={alpha} alt="alpha Logo" className="alpha-logo" />
+          <img src={alpha} alt="alpha Logo" className="alpha-logo"/>
           <span className="beta-tag">Beta</span>
         </header>
       </div>
@@ -157,22 +158,21 @@ function App() {
           />
         </div>
         <div className="sidebar-app">
-          {isSidebarOpen ? (
+          <div className={clsx('sidebar', {'open': isSidebarOpen}, {'sidebar-light': isLightMode})}>
             <Sidebar
+              isSidebarOpen={isSidebarOpen}
               isLightMode={isLightMode}
               handleReset={handleReset}
               allChats={allChats}
               showChat={showChat}
             />
-          ) : (
-            ""
-          )}
+          </div>
           <button
-            className={`sidebar-toggle ${isSidebarOpen ? "open" : ""}`}
+            className={`sidebar-toggle ${isSidebarOpen ? 'open' : ''}`}
             onClick={toggleSidebar}
             data-testid="sidebar-toggle"
           >
-            <PiLineVerticalBold />
+            <PiLineVerticalBold/>
           </button>
         </div>
         <UserIcon
